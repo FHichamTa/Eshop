7f8cf987ffd53aaf91c6b5e7ba7a49efde9846ef
Johan le Roch <johanleroch@gmail.com>
2024-02-01 09:27:29 +0100

    Init header

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link href="/">
-        <a>Home</a> 
-      </Link>
+    <nav className="border-b shadow-md py-4">
+      <div className="max-w-7xl mx-auto px-4 flex justify-between">
+        <Link href="/">Home</Link>
+        <div>
+          <span>Mon panier</span>
+        </div>
+      </div>
     </nav>
   );
 };
