ec507cc4ccd1401d052b63b931844c98d1c59a59
Hicham <gitlab.icg@gmail.com>
2024-02-01 09:19:42 +0100

    fix

---
+++ src/app/navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link href="/">
+        <a>Home</a> 
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/app/page.tsx
@@ -0,0 +1,48 @@
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import Image from "next/image";
+import { cookies } from "next/headers";
+import Navbar from "./navbar";
+
+// La page ici est process côté serveur
+export default async function Home() {
+  const cookieStore = cookies();
+
+  // on utilise ici le client supabase côté serveur
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+      },
+    }
+  );
+
+  const products = await supabase
+    .from("products")
+    .select("*, productImages(*), models(name)");
+
+  return (
+    <div>
+      <Navbar /> {/* Include the Navbar component here */}
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <ul className="flex gap-4">
+          {products.data?.map((product) => (
+            <li key={product.id}>
+              <div className="rounded-lg overflow-hidden bg-red-200 w-[250px] h-[250px]">
+                <h1>{product.models.name}</h1>
+                <img
+                  className="w-full h-full object-cover"
+                  alt={`Shoe image for product id ${product.id}`}
+                  src={product.productImages[0]?.url}
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
