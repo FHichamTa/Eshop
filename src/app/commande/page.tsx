c9eb0e962949bd0d521da32038fb380ee0f644dc
KoneNeil <150436289+KoneNeil@users.noreply.github.com>
2024-02-02 17:50:18 +0100

    style

--- src/app/commande/page.tsx
+++ src/app/commande/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Link from 'next/link';
 import React from "react"
 
 
@@ -146,6 +147,10 @@ export default function App() {
       noValidate
     >
       <div>
+      <Link href="/"><button type="button" className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">Home</button></Link>
+      </div>
+      <br></br>
+      <div>
         <label htmlFor={nameId}>Nom et prénom</label>
         <input
           required
@@ -284,7 +289,8 @@ export default function App() {
           </fieldset>
         </div>
       </div>
-      <button type="submit">Commander</button>
+      <br></br>
+      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Commander</button>
     </form>
   )
 }
