3f2ae9b24ba062b7f08be063e5f8d59a23d929ef
KoneNeil <koneneil@gmail.com>
2024-02-05 21:30:54 +0100

    menu add

--- src/app/jordan1lucky/page.tsx
+++ src/app/jordan1lucky/page.tsx
@@ -19,7 +19,8 @@ const SneakersList = () => {
     <div className="max-w-md mx-auto p-4 border rounded-md shadow-lg bg-white">
       <div className="mb-4">
         <Link href="/"><button type="button" className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">Home</button></Link>
-        <p>
+        <h1 className="text-2xl font-semibold mt-4">Air Jordan 1 Retro High OG "Lucky Green"</h1>
+        <p className="mt-2">
           La Air Jordan 1 Retro High OG "Lucky Green" incarne l'essence de l'audace et de l'élégance, fusionnant le patrimoine emblématique de la ligne Jordan avec une palette de couleurs vives et captivantes. Cette sneaker légendaire offre une interprétation contemporaine du design classique, capturant l'admiration des amateurs de sneakers et des passionnés de mode.
         </p>
       </div>
